293d353f3437d9b2db6cca0240bcd0155af85216
Barak Rapke <rapkeb@Baraks-MacBook-Pro.local>
2024-06-05 16:15:58 +0300

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_uJ3xMZM3i
+        userPoolClientId: '', // e.g. 7s85se7a3j1b02bj3uqvep77ip
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
